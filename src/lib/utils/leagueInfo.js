5b0385e170e33e8e32dae9ebd9ae35d18d84ef62
Trevo21 <trevorcarter8565@gmail.com>
2026-08-01 16:41:29 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -155,7 +155,7 @@ export const managers = [
       "favoriteTeam": "jax", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Cruit", // Can be anything (usually your rival's name)
+        name: "Mediocrity", // Can be anything (usually your rival's name)
         link: null, // manager array number within this array, or null to link back to all managers page
         image: "/managers/mediocrity.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
