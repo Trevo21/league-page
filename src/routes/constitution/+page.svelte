d800f51f145678500336e8818c61dd390ea7949e
Trevo21 <trevorcarter8565@gmail.com>
2026-08-01 13:43:37 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,6 +1,8 @@
 <div class="constitution">
-<style="text-align: center">
-<iframe src="https://docs.google.com/document/d/e/2PACX-1vRwbGVRctpu80Dn5DB23sf_36GUuINp_MV-qxm9PY1Dfy5FVxFp2y_7GRspb4VGz-lziilnsqJp5X06/pub?embedded=true"></iframe>
+<iframe 
+src="https://docs.google.com/document/d/e/2PACX-1vRwbGVRctpu80Dn5DB23sf_36GUuINp_MV-qxm9PY1Dfy5FVxFp2y_7GRspb4VGz-lziilnsqJp5X06/pub?embedded=true">
+
+</iframe>
 </div>
 
 /*<script>
