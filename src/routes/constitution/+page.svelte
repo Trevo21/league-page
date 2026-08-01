10a6ba3c717038d6b03aebf2a1c80de0c51f8a3f
Trevo21 <trevorcarter8565@gmail.com>
2026-08-01 13:58:43 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -2,6 +2,6 @@
 <iframe 
 src="https://docs.google.com/document/d/e/2PACX-1vRwbGVRctpu80Dn5DB23sf_36GUuINp_MV-qxm9PY1Dfy5FVxFp2y_7GRspb4VGz-lziilnsqJp5X06/pub?embedded=true">
 width="100%"
-height="100%"
+height="100%">
 </iframe>
 </div>
