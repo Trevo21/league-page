3890ed264b4ae6cd2a877175886f826cb55899ae
Trevo21 <trevorcarter8565@gmail.com>
2026-08-08 17:37:40 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -31,7 +31,7 @@ export const managers = [
       "location": "Jacksonville", // (optional)
       "bio": "Long time co-manager that replaced Freddy when he retired, First Woman in the League",
       "photo": "/managers/trevor.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2009, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2005, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "jax", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
