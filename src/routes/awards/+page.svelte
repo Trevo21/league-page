65ffac0b55e839c6e76381946a0822f4654a4bda
Trevo21 <trevorcarter8565@gmail.com>
2026-08-16 16:16:47 -0400

    Update +page.svelte

--- src/routes/awards/+page.svelte
+++ src/routes/awards/+page.svelte
@@ -403,17 +403,15 @@
 	</div>
 </div>
 
-		Toilet Bowl -->
-	<!-- {#if toilet}
-		<div class="toiletParent">
+		<!-- Toilet Bowl -->
+<div class="toiletParent">
 			
-			<img src="/toilet-banner.png" class="toilet-banner" alt="The Toilet Bowl" />
+	<img src="/toilet-banner.png" class="toilet-banner" alt="The Toilet Bowl" />
 
-			<div class="toiletBowl">
-				<img src="{getAvatarFromTeamManagers(leagueTeamManagers, toilet, year)}" class="toiletWinner clickable" onclick={() => gotoManager({year, leagueTeamManagers, rosterID: toilet})} alt="toilet bowl winner" />
-				<img src="/toilet-bowl-2.png" class="toilet" alt="toilet bowl" />
-			</div>
-			<span class="genLabel clickable" onclick={() => gotoManager({year, leagueTeamManagers, rosterID: toilet})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, toilet)}</span>
+		<div class="toiletBowl">
+			<img src="/managers/james.jpg" class="toiletWinner" alt="toilet bowl winner" />
+			<img src="/toilet-bowl-2.png" class="toilet" alt="toilet bowl" />
 		</div>
-	{/if}
-</div> -->
+		<span class="genLabel" >James</span>
+	</div>
+</div>
