b12021cdccc5d9978802aa9da7866a8c32813bd1
Trevo21 <trevorcarter8565@gmail.com>
2026-08-16 18:31:30 -0400

    Update +page.svelte

--- src/routes/awards/+page.svelte
+++ src/routes/awards/+page.svelte
@@ -71,8 +71,8 @@
 
 	.champ {
 		position: absolute;
-		width: 87px;
-		height: 87px;
+		width: 122px;
+		height: 122px;
 		transform: translate(-50%, -50%);
 		border-radius: 100%;
 		border: 1px solid var(--bbb);
