8733878406cfbab3679fd159a076f1e3f44ee556
Trevo21 <trevorcarter8565@gmail.com>
2026-08-20 18:04:30 -0400

    Update +page.svelte

--- src/routes/histrecords/+page.svelte
+++ src/routes/histrecords/+page.svelte
@@ -1,4 +1,7 @@
 <div class="constitution" style="text-align: center">
-<iframe align="middle" title="Night Fishin' Record and 'Ships" aria-label="Table" id="datawrapper-chart-Yv1VO" src="https://datawrapper.dwcdn.net/Yv1VO/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="572" data-external="1"></iframe>
+<iframe align="middle" title="Night Fishin' Record and 'Ships" aria-label="Table" id="datawrapper-chart-Yv1VO" src="https://datawrapper.dwcdn.net/Yv1VO/1/" scrolling="no" frameborder="0" 
+style="width: 700; min-width: 100% !important; border: none;" 
+height="572" 
+data-external="1"></iframe>
 <script type="text/javascript">(function(){function e(){window.addEventListener(`message`,function(e){if(e.data[`datawrapper-height`]!==void 0){var t=document.querySelectorAll(`iframe`);for(var n in e.data[`datawrapper-height`])for(var r=0,i;i=t[r];r++)if(i.contentWindow===e.source){var a=e.data[`datawrapper-height`][n]+`px`;i.style.height=a}}})}e()})();</script>
 </div>
